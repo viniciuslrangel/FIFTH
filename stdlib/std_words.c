ecfdd4d82db0b3821e874f811b53308a51ac8c94
Vinicius Rangel <vinicius.l.rangel@gmail.com>
2018-09-08 21:27:43 -0300

    Add NSWITCH and NNSWITCH words Updated DUP to use peek instead of pop

--- stdlib/std_words.c
+++ stdlib/std_words.c
@@ -29,8 +29,7 @@ void stdlib_println(ProgramStack stack) {
 }
 
 void stdlib_dup(ProgramStack stack) {
-    struct PElement e = PStack_pop(stack);
-    PStack_push(stack, e);
+    struct PElement e = PStack_peek(stack);
     if(e.type == DATATYPE_STRING) {
         char* copy = malloc(strlen(e.data.string) + 1);
         strcpy(copy, e.data.string);
@@ -60,12 +59,40 @@ void stdlib_tonum(ProgramStack stack) {
     }
 }
 
+void stdlib_nswitch(ProgramStack stack) {
+    number_t index;
+    POP_NUMBER(index, stack);
+
+    struct PElement e1 = PStack_popIndex(stack, (unsigned int) index);
+    struct PElement e2 = PStack_pop(stack);
+    PStack_push(stack, e1);
+    PStack_pushIndex(stack, e2, (unsigned int) index);
+}
+
+void stdlib_nnswitch(ProgramStack stack) {
+    number_t i1, i2;
+    POP_NUMBER(i1, stack);
+    POP_NUMBER(i2, stack);
+    if(i1 < i2) {
+        number_t tmp = i1;
+        i1 = i2;
+        i2 = tmp;
+    }
+
+    struct PElement e1 = PStack_popIndex(stack, (unsigned int) i1);
+    struct PElement e2 = PStack_popIndex(stack, (unsigned int) i2);
+    PStack_pushIndex(stack, e1, (unsigned int) i2);
+    PStack_pushIndex(stack, e2, (unsigned int) i1);
+}
+
 void RegisterStdWords() {
     struct WordEntry wordEntry[] = {
                 {"PRINT", stdlib_print},
                 {"PRINTLN", stdlib_println},
                 {"DUP", stdlib_dup},
                 {"SWITCH", stdlib_switch},
+                {"NSWITCH", stdlib_nswitch},
+                {"NNSWITCH", stdlib_nnswitch},
                 {"TONUM", stdlib_tonum}
         };
     RegisterWords(wordEntry, sizeof(wordEntry) / sizeof(struct WordEntry));
